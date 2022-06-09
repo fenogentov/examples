02f2a0199f91812e000ae1427f17253e3f35c71f
fenogentov <ShuraF@yandex.ru>
2022-06-09 10:29:18 +0300

    change order check in case

--- mylinter/check/testdata/src/p/p.go
+++ mylinter/check/testdata/src/p/p.go
@@ -5,7 +5,69 @@ import (
 	"time"
 )
 
-func examp(v interface{}) {
+func switchOK(v interface{}) {
+	switch v := v.(type) {
+	// case *types.Document:
+	// 	fmt.Println(v)
+	// case *types.Array:
+	// 	fmt.Println(v)
+	case float64:
+		fmt.Println(v)
+	case string:
+		fmt.Println(v)
+		// case types.Binary:
+		// 	fmt.Println(v)
+		// case types.ObjectID:
+		// 	fmt.Println(v)
+	case bool:
+		fmt.Println(v)
+	case time.Time:
+		fmt.Println(v)
+	// case types.NullType:
+	// 	fmt.Println(v)
+	// case types.Regex:
+	// 	fmt.Println(v)
+	case int32:
+		fmt.Println(v)
+	// case types.Timestamp:
+	// 	fmt.Println(v)
+	case int64:
+		fmt.Println(v)
+	default:
+		fmt.Println(v)
+	}
+}
+
+func severalConditionsCaseOK(v interface{}) {
+	switch v := v.(type) {
+	// case *types.Document:
+	// 	fmt.Println(v)
+	// case *types.Array:
+	// 	fmt.Println(v)
+	case float64, int32, int64:
+		fmt.Println(v)
+	case string:
+		fmt.Println(v)
+		// case types.Binary:
+		// 	fmt.Println(v)
+		// case types.ObjectID:
+		// 	fmt.Println(v)
+	case bool:
+		fmt.Println(v)
+	case time.Time:
+		fmt.Println(v)
+	// case types.NullType:
+	// 	fmt.Println(v)
+	// case types.Regex:
+	// 	fmt.Println(v)
+	// case types.Timestamp:
+	// 	fmt.Println(v)
+	default:
+		fmt.Println(v)
+	}
+}
+
+func switchWrong(v interface{}) {
 	switch v := v.(type) { // want "non-observance of the preferred order of types"
 	case float64:
 		fmt.Println(v)
@@ -19,7 +81,6 @@ func examp(v interface{}) {
 	// 	fmt.Println(v)
 	// case types.ObjectID:
 	// 	fmt.Println(v)
-
 	case time.Time:
 		fmt.Println(v)
 	case bool:
@@ -34,6 +95,64 @@ func examp(v interface{}) {
 	// 	fmt.Println(v)
 	case int64:
 		fmt.Println(v)
+	default:
+		fmt.Println(v)
+	}
+}
+
+func severalConditionsCaseWrong(v interface{}) {
+	switch v := v.(type) { // want "non-observance of the preferred order of types"
+	// case *types.Document:
+	// 	fmt.Println(v)
+	// case *types.Array:
+	// 	fmt.Println(v)
+	case float64, int64, int32:
+		fmt.Println(v)
+	case string:
+		fmt.Println(v)
+		// case types.Binary:
+		// 	fmt.Println(v)
+		// case types.ObjectID:
+		// 	fmt.Println(v)
+	case bool:
+		fmt.Println(v)
+	case time.Time:
+		fmt.Println(v)
+	// case types.NullType:
+	// 	fmt.Println(v)
+	// case types.Regex:
+	// 	fmt.Println(v)
+	// case types.Timestamp:
+	// 	fmt.Println(v)
+	default:
+		fmt.Println(v)
+	}
+}
+
+func severalWrong(v interface{}) {
+	switch v := v.(type) { // want "non-observance of the preferred order of types"
+	// case *types.Document:
+	// 	fmt.Println(v)
+	// case *types.Array:
+	// 	fmt.Println(v)
+	case float64, int32, int64:
+		fmt.Println(v)
+	case bool:
+		fmt.Println(v)
+	case string:
+		fmt.Println(v)
+	// case types.Binary:
+	// 	fmt.Println(v)
+	// case types.ObjectID:
+	// 	fmt.Println(v)
+	case time.Time:
+	// 	fmt.Println(v)
+	// case types.NullType:
+	// 	fmt.Println(v)
+	// case types.Regex:
+	// 	fmt.Println(v)
+	// case types.Timestamp:
+	// 	fmt.Println(v)
 	default:
 		fmt.Println(v)
 	}
